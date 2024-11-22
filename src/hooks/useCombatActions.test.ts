ab798efd3ef013612f70d79917d3c75f97db5638
Gabriel Ávila <avilabiel@gmail.com>
2024-11-22 20:00:20 -0300

    so now we have better test names

--- src/hooks/useCombatActions.test.ts
+++ src/hooks/useCombatActions.test.ts
@@ -2,41 +2,72 @@ import { renderHook } from "@testing-library/react";
 import { useCombatActions } from "./useCombatActions";
 import { act } from "react-dom/test-utils";
 
-it("baseAtk: verifica se o hp do receiver muda quando é atacado ", () => {
-  const { result } = renderHook(() => useCombatActions());
-  const { baseAtk, user, monster } = result.current;
-  const initialMonsterHp = monster.getHp();
-  act(() => baseAtk(user, monster));
-  expect(monster.getHp()).toBeLessThan(initialMonsterHp);
-});
+describe("baseAtk", () => {
+  const windowMock = jest.spyOn(window, "alert").mockImplementation(() => {});
 
-it("baseAtk: verifica se o hp do sender muda quando é atacado", () => {
-  const { result } = renderHook(() => useCombatActions());
-  const { baseAtk, monster } = result.current;
-  const initialUserHp = result.current.user.getHp();
-  act(() => baseAtk(monster, result.current.user));
-  expect(result.current.user.getHp()).toBeLessThan(initialUserHp);
-});
-it("baseAtk: receiver morre quando  HP chega a 0", () => {
-  const { result } = renderHook(() => useCombatActions());
-  const { baseAtk, monster } = result.current;
-  monster.setHp(0);
-  baseAtk(result.current.user, monster);
-  expect(monster.getHp()).toBeLessThanOrEqual(0);
-});
-it("baseAtk: sender morre quando  HP chega a 0", () => {
-  const { result } = renderHook(() => useCombatActions());
-  const { baseAtk, user } = result.current;
-  user.setHp(0);
-  act(() => baseAtk(result.current.monster, user));
-  expect(user.getHp()).toBeLessThanOrEqual(0);
-});
-it("baseAtk: receiver ataca sender quando não está morto", () => {
-  const { result } = renderHook(() => useCombatActions());
-  const { baseAtk, user, monster } = result.current;
-  const initialUserHp = user.getHp();
-  monster.setHp(100);
-  act(() => baseAtk(user, monster));
-  expect(user.getHp()).toBeLessThan(initialUserHp);
-  expect(monster.getHp()).toBeGreaterThan(0);
+  it("decreases monster's hp", () => {
+    // Arrange
+    const { result } = renderHook(() => useCombatActions());
+    const { baseAtk, user, monster } = result.current;
+    const initialMonsterHp = monster.getHp();
+
+    // Act
+    act(() => baseAtk(user, monster));
+
+    // Assert
+    expect(monster.getHp()).toBeLessThan(initialMonsterHp);
+  });
+
+  it("decreases user's hp", () => {
+    const { result } = renderHook(() => useCombatActions());
+    const { baseAtk, monster, user } = result.current;
+    const initialUserHp = user.getHp();
+
+    act(() => baseAtk(monster, user));
+
+    expect(user.getHp()).toBeLessThan(initialUserHp);
+  });
+
+  it("fires an alert when monster is dead", () => {
+    const { result } = renderHook(() => useCombatActions());
+    const { baseAtk, monster, user } = result.current;
+    monster.setHp(0);
+
+    act(() => baseAtk(user, monster));
+
+    expect(windowMock).toBeCalled();
+  });
+
+  it("fires an alert when user is dead", () => {
+    const { result } = renderHook(() => useCombatActions());
+    const { baseAtk, user, monster } = result.current;
+
+    act(() => baseAtk(user, monster));
+
+    expect(windowMock).toBeCalled();
+  });
+
+  it("does not allow monster to strike back if it is dead", () => {
+    const { result } = renderHook(() => useCombatActions());
+    const { baseAtk, monster, user } = result.current;
+    monster.setHp(0);
+    const initialUserHp = user.getHp();
+
+    act(() => baseAtk(user, monster));
+
+    expect(monster.getHp()).toBeLessThanOrEqual(0);
+    expect(user.getHp()).toEqual(initialUserHp);
+  });
+
+  it("does allow monster to strike back if it's still alive", () => {
+    const { result } = renderHook(() => useCombatActions());
+    const { baseAtk, user, monster } = result.current;
+    const initialUserHp = user.getHp();
+    monster.setHp(100);
+
+    act(() => baseAtk(user, monster));
+
+    expect(user.getHp()).toBeLessThan(initialUserHp);
+    expect(monster.getHp()).toBeGreaterThan(0);
+  });
 });
