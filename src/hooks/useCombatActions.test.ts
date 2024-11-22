f765023c7f46f12019d6125813a25acf95493860
João <joao.braga.porto01@gmail.com>
2024-11-22 10:45:46 -0300

    unit tests in BaseAtk

--- src/hooks/useCombatActions.test.ts
+++ src/hooks/useCombatActions.test.ts
@@ -1,11 +1,12 @@
 import { renderHook } from "@testing-library/react";
 import { useCombatActions } from "./useCombatActions";
+import { act } from "react-dom/test-utils";
 
 it("baseAtk: verifica se o hp do receiver muda quando é atacado ", () => {
   const { result } = renderHook(() => useCombatActions());
   const { baseAtk, user, monster } = result.current;
   const initialMonsterHp = monster.getHp();
-  baseAtk(user, monster);
+  act(() => baseAtk(user, monster));
   expect(monster.getHp()).toBeLessThan(initialMonsterHp);
 });
 
@@ -13,7 +14,7 @@ it("baseAtk: verifica se o hp do sender muda quando é atacado", () => {
   const { result } = renderHook(() => useCombatActions());
   const { baseAtk, monster } = result.current;
   const initialUserHp = result.current.user.getHp();
-  baseAtk(monster, result.current.user);
+  act(() => baseAtk(monster, result.current.user));
   expect(result.current.user.getHp()).toBeLessThan(initialUserHp);
 });
 it("baseAtk: receiver morre quando  HP chega a 0", () => {
@@ -27,6 +28,15 @@ it("baseAtk: sender morre quando  HP chega a 0", () => {
   const { result } = renderHook(() => useCombatActions());
   const { baseAtk, user } = result.current;
   user.setHp(0);
-  baseAtk(result.current.monster, user);
+  act(() => baseAtk(result.current.monster, user));
   expect(user.getHp()).toBeLessThanOrEqual(0);
+});
+it("baseAtk: receiver ataca sender quando não está morto", () => {
+  const { result } = renderHook(() => useCombatActions());
+  const { baseAtk, user, monster } = result.current;
+  const initialUserHp = user.getHp();
+  monster.setHp(100);
+  act(() => baseAtk(user, monster));
+  expect(user.getHp()).toBeLessThan(initialUserHp);
+  expect(monster.getHp()).toBeGreaterThan(0);
 });
