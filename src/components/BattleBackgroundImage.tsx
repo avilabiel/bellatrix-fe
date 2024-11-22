9e3eb85c56e6289324466c32285a8d667b57bc4b
João <joao.braga.porto01@gmail.com>
2024-11-22 10:52:19 -0300

    compressed images

--- src/components/BattleBackgroundImage.tsx
+++ src/components/BattleBackgroundImage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 type Props = { children: React.ReactNode };
 const BattleBackgroundImage = ({ children }: Props) => {
   return (
-    <div className='bg-[url("./public/images/bgDesert.png")] absolute w-full h-full bg-no-repeat bg-cover flex flex-col justify-center '>
+    <div className='bg-[url("./public/images/bgDesert.png")] absolute w-full h-full bg-no-repeat bg-cover bg-center flex flex-col justify-center '>
       {children}
     </div>
   );
