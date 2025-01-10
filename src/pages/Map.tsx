d8f3ae4684286f45947f44302547b70cc9d5b1b3
João <joao.braga.porto01@gmail.com>
2025-01-10 18:30:36 -0300

    new map component

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
-import x from "../public/images/map.png";
 import Character from "@/components/Character";
 import { useCharacter } from "@/hooks/useWalk";
 const Map = () => {
-  const char = useCharacter("Juan");
+  const char = useCharacter("x");
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
   }, []);
   const handleKeyDown = (e: KeyboardEvent) => {
+    console.log(e.code)
     switch (e.code) {
       case "KeyA":
       case "ArrowLeft":
@@ -28,7 +28,10 @@ const Map = () => {
     }
   };
   return (
-    <div id="map-image" className="bg-[url('./public/images/map.png')] absolute top-0 left-0 w-full h-full bg-cover">
+    <div
+      id="map-image"
+      className="bg-[url('./public/images/map.png')] absolute top-0 left-0 w-full h-full bg-cover"
+    >
       <Character x={char.x} y={char.y} name={char.name} />
     </div>
   );
