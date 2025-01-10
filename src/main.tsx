d13bb22accb91badad386d2462ad6d46fb158404
João <joao.braga.porto01@gmail.com>
2025-01-09 21:44:13 -0300

    new route map in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from '@/app'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "@/app";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Map from "./pages/Map";
 
-const rootElement = document.querySelector('[data-js="root"]')
+const rootElement = document.querySelector('[data-js="root"]');
 
 if (!rootElement) {
-  throw new Error('Failed to find the root element')
+  throw new Error("Failed to find the root element");
 }
 
-const root = createRoot(rootElement)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/map",
+    element: <Map />,
+  },
+]);
+
+const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
